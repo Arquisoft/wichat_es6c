a452d6ba6c53aee03de030e31af2643448770b54
Germán García de la Llana <germangarciadelallana@gmail.com>
2025-02-24 23:04:59 +0100

    NavBar
    Se ha creado un menu de navegeación con las opciones ver historico, perfil, ajustes y cerrar sesión

---
+++ webapp/src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Divider, Box } from "@mui/material";
+import { MoreVert, History, Person, Settings, Logout } from "@mui/icons-material";
+import logo from "../logo.svg";
+const NavMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
+  const handleMenuClose = () => setAnchorEl(null);
+
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        
+         
+         <Box sx={{ display: "flex", alignItems: "center" }}>
+          <img src={logo} alt="Logo" style={{ width: 40, height: 40, marginRight: 10 }} />
+          <Typography variant="h6">WICHAT</Typography>
+        </Box>
+
+       
+        <Box>
+          <IconButton color="inherit" onClick={handleMenuOpen}>
+            <MoreVert />
+          </IconButton>
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+            <MenuItem onClick={handleMenuClose}>
+              <Person sx={{ mr: 1 }} /> Perfil
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose}>
+              <Settings sx={{ mr: 1 }} /> Ajustes
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleMenuClose}>
+              <History sx={{ mr: 1 }} /> Ver Histórico
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleMenuClose}>
+              <Logout sx={{ mr: 1 }} /> Cerrar Sesión
+            </MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavMenu;
--- webapp/src/pages/UserHome.js
+++ webapp/src/pages/UserHome.js
@@ -1,40 +1,14 @@
-import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Button, Box } from "@mui/material";
-import { MoreVert, History, Person, Settings } from "@mui/icons-material";
+import React from "react";
+import { Box, Button } from "@mui/material";
+import NavBar from "../components/NavBar"; // Importa la barra de navegación
 
 const HomePage = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
-  const handleMenuClose = () => setAnchorEl(null);
-
   return (
     <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-      {/* Navbar */}
-      <AppBar position="static">
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-         
-          <Typography variant="h6">WICHAT</Typography>
-
-          {/* Menú */}
-          <IconButton color="inherit" onClick={handleMenuOpen}>
-            <MoreVert />
-          </IconButton>
-          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-            <MenuItem onClick={handleMenuClose}>
-              <History sx={{ mr: 1 }} /> Ver Histórico
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Person sx={{ mr: 1 }} /> Perfil
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Settings sx={{ mr: 1 }} /> Ajustes
-            </MenuItem>
-          </Menu>
-        </Toolbar>
-      </AppBar>
-
      
+      <NavBar />
+
+      
       <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Button variant="contained" size="large" sx={{ fontSize: "1.2rem", px: 5, py: 2 }}>
           Jugar
