c44818fc56ff213bdb5bb303c5899677d5853a10
Jorge-Puente-García <113630985+JorgeePG@users.noreply.github.com>
2025-03-31 20:42:05 +0200

    Hecho el merge con develop
    He juntado el código con el de develop

--- arrancaTodo.bat
+++ arrancaTodo.bat
@@ -2,7 +2,7 @@
 
 start "GatewayService" cmd /k "cd gatewayservice && npm install && npm start"
 start "LLMService" cmd /k "cd llmservice && npm install && npm start"
-start "QuestionsService" cmd /k "cd questions && npm install && npm start"
+start "QuestionsService" cmd /k "cd questionservice && npm install && npm start"
 start "AuthService" cmd /k "cd users\authservice && npm install && npm start"
 start "UserService" cmd /k "cd users\userservice && npm install && npm start"
 start "HistoryService" cmd /k "cd users\historyservice && npm install && npm start"
