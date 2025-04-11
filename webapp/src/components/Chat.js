07d98219215c87ede5a0758c90f1ec6255c78203
Germán García de la Llana <germangarciadelallana@gmail.com>
2025-04-11 17:46:48 +0200

    Cambio docker y page Chat

--- webapp/src/components/Chat.js
+++ webapp/src/components/Chat.js
@@ -7,7 +7,6 @@ function Chat({ questionData }) {
   const [input, setInput] = useState("");
   //const [model, setModel] = useState("empathy");
   const [isTyping, setIsTyping] = useState(false); // Indicador para mostrar que el bot está escribiendo
-  const API_KEY = process.env.REACT_APP_LLM_API_KEY; // Usa .env en producción
   const messagesEndRef = useRef(null); // Ref para hacer scroll al final
 
   const sendMessage = async () => {
@@ -29,7 +28,6 @@ function Chat({ questionData }) {
         `${apiEndpoint}/askllm`,
         {
           question: petition,
-          apiKey: API_KEY,
           model: "empathy"
         },
         {
