a563486419a04f85c6054eeb044c9f781dfc4b29
uo288406 <clau1600@gmail.com>
2025-04-18 01:05:47 +0200

    aplicacion movil

---
+++ wichatApp/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Platform, StatusBar, SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <WebView
+        source={{ uri: 'http://48.209.11.56:3000/' }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
